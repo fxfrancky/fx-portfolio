4cf67171d03aa98def85fd1562d52380721ff753
Franck Owen <fxfrancky@yahoo.com>
2022-09-04 01:42:51 +0200

    First Responsive behavior

--- js/main.js
+++ js/main.js
@@ -1,22 +1,7 @@
 /* Refresh a div */
 function updateDiv() {
   $("#section-skills").load(window.location.href + " #section-skills");
-  // $("#circular-progress").load(window.location.href + " #circular-progress");
 }
-
-// JavaScript animation for link or button
-$(".link").on("click", function (event) {
-  if (this.hash !== "") {
-    event.preventDefault();
-    const hash = this.hash;
-    $("html, body").animate(
-      {
-        scrollTop: $(hash).offset().top,
-      },
-      800
-    );
-  }
-});
 
 // When we scroll down we want to have an opacity on the navbar menu
 //but no when its on top
