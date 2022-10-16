77078626f09f5adf9c2127ec37c2e44f51674bad
Franck Owen <fxfrancky@yahoo.com>
2022-10-16 23:09:37 +0200

    Commit portfolio v2

--- js/main.js
+++ js/main.js
@@ -1,29 +1,6 @@
-/* Refresh a div */
-function updateDiv() {
-  $("#section-skills").load(window.location.href + " #section-skills");
-}
-
-// When we scroll down we want to have an opacity on the navbar menu
-//but no when its on top
-window.addEventListener("scroll", function () {
-  if (window.scrollY > 150) {
-    document.querySelector(".navbar").style.opacity = 0.95;
-  } else {
-    document.querySelector(".navbar").style.opacity = 1;
-  }
-});
-
-// JQuery Smooth Scroll
-$(".navbar a").on("click", function (e) {
-  if (this.hash != "") {
-    e.preventDefault();
-
-    const hash = this.hash;
-    $("html, body").animate(
-      {
-        scrollTop: $(hash).offset().top,
-      },
-      800
-    );
-  }
+// Change nav style on scroll
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("#nav-header")
+    .classList.toggle("window-scrolled", window.scrollY > 0);
 });
---
+++ js/mobile-menu.js
@@ -0,0 +1,24 @@
+// Mobile Menu
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
+// Mobile Menu Event
+btn.addEventListener("click", navToggle);
+
+// END JAVASCRIPT FORM VALIDATION
+// Toggle Mobile Menu
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+}
+
+(function ($) {
+  "use strict";
+
+  if (matchMedia("only screen and (max-width: 1024px)").matches) {
+    $(".menu li a").on("click", function () {
+      $(".sydney-offcanvas-menu").removeClass("toggled");
+    });
+  }
+})(jQuery);
